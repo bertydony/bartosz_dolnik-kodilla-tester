8ee6e11d000fd8fa19dfd9e494d66f94d86d7272
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2022-07-23 17:41:20 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "To jest przyklad";
+        System.out.println(text);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Bartosz";
+        System.out.println(myName);
+        int myAge = 24;
+        System.out.println(myAge);
+        double height = 1.81;
+        System.out.println(height);
+        char myLetter= 'V';
+        System.out.println(myLetter);
+        String text = "Będzie dobrze?";
+        System.out.println(text);
+        boolean isBadProgrammer = true;
+        System.out.println(isBadProgrammer);
+
+
+    }
+}
