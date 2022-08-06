721b57e65d5b9c1dbb54dfa498dab307b2e7ed02
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2022-08-06 09:58:07 +0200

    Initial version

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,25 +1,16 @@
 import java.util.Scanner;
-
-
 public class LeapYear {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Podaj rok");
         int rok = scanner.nextInt();
 
         boolean przestepny = true;
-
-
         System.out.println("Podaj rok");
-
         if ((rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0) {
             System.out.println(true);
         } else {
             System.out.println(false);
-
-
         }
     }
 }
