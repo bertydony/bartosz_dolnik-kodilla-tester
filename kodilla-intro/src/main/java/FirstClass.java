a6c6f7bad7e92dfcd1a10920a9d21af19a21241b
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2022-08-13 15:11:36 +0200

    Initial version

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,8 +1,23 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
-        if (1 > 2){
-            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        Notebook notebook = new Notebook(600, 2000, 2020);
+        System.out.println("Waga: " + notebook.weight + " " + "Cena: " +  notebook.price + " " + "Rok produkcji: " +  notebook.year);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkPriceAndYear();
+
+        Notebook heavyNotebook = new Notebook (2000, 1500, 2015);
+        System.out.println("Waga: " + heavyNotebook.weight + " " + "Cena: " +  heavyNotebook.price + " " + "Rok produkcji: " +  heavyNotebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkPriceAndYear();
+
+        Notebook oldNotebook = new Notebook (1600, 500,2002);
+        System.out.println("Waga: " + oldNotebook.weight + " " + "Cena: " +  oldNotebook.price + " " + "Rok produkcji: " + oldNotebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkPriceAndYear();
+
+
         }
     }
-}
