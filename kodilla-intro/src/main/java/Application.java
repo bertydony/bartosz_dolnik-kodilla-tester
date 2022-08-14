ba2408751533d3192cf94bacdb962e1406ee604a
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2022-08-14 17:47:56 +0200

    Initial version

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -2,20 +2,22 @@ public class Application {
     public String name;
     public double age;
     public double height;
+
     public Application(String name, double age, double height) {
         this.name = name;
         this.age = age;
         this.height = height;
     }
-        public static void main(String[] args){
-        Application adam = new Application("Adam",40.5,178 );
-            if (adam.name != null) {
-                if (adam.age > 30 && adam.height > 160) {
-                    System.out.println("User is older than 30 and taller than 160cm");
-                } else {
-                    System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-                }
+
+    public static void main(String[] args) {
+        Application adam = new Application("Adam", 40.5, 178);
+        if (adam.name != null) {
+            if (adam.age > 30 && adam.height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
             }
         }
+    }
 
 }
