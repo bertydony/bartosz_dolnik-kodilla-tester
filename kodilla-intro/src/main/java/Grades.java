5d1b4db465006eb47d51efb76ee30d071d75f3da
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2022-08-14 17:46:43 +0200

    Initial version

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -2,29 +2,31 @@ public class Grades {
     private int[] grades;
     private int tabElement;
 
-    public Grades(){
+    public Grades() {
         this.grades = new int[10];
         this.tabElement = 0;
     }
-    public void add(int value){
-        if(this.tabElement == 10){
+
+    public void add(int value) {
+        if (this.tabElement == 10) {
             return;
         }
         this.grades[this.tabElement] = value;
         this.tabElement++;
     }
-    public int[] lastGrade(){
-        if(this.tabElement>0) {
+
+    public int[] lastGrade() {
+        if (this.tabElement > 0) {
             return grades;
         }
         return new int[0];
     }
+
     public double averange() {
-        int sum=0;
-        for(int i=0; i< grades.length;i++){
-            sum=sum+ grades.length;
-        } return sum/grades.length;
-
-
+        int sum = 0;
+        for (int i = 0; i < grades.length; i++) {
+            sum = sum + grades.length;
+        }
+        return sum / grades.length;
     }
 }
