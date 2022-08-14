3d98f2f1a627b9e62aee10138141cdd341b7fca8
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2022-08-14 14:28:49 +0200

    Initial version

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,21 +1,17 @@
 public class FirstClass {
     public static void main(String[] args) {
         Notebook notebook = new Notebook(600, 2000, 2020);
-        System.out.println("Waga: " + notebook.weight + " " + "Cena: " +  notebook.price + " " + "Rok produkcji: " +  notebook.year);
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2015);
+        Notebook oldNotebook = new Notebook(1600, 500, 2002);
+
+        notebookParameters(notebook);
+        notebookParameters(heavyNotebook);
+        notebookParameters(oldNotebook);
+    }
+    private static void notebookParameters(Notebook notebook) {
+        System.out.println("Waga: " + notebook.weight + " " + "Cena: " + notebook.price + " " + "Rok produkcji: " + notebook.year);
         notebook.checkPrice();
         notebook.checkWeight();
         notebook.checkPriceAndYear();
-
-        Notebook heavyNotebook = new Notebook (2000, 1500, 2015);
-        System.out.println("Waga: " + heavyNotebook.weight + " " + "Cena: " +  heavyNotebook.price + " " + "Rok produkcji: " +  heavyNotebook.year);
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.checkPriceAndYear();
-
-        Notebook oldNotebook = new Notebook (1600, 500,2002);
-        System.out.println("Waga: " + oldNotebook.weight + " " + "Cena: " +  oldNotebook.price + " " + "Rok produkcji: " + oldNotebook.year);
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.checkPriceAndYear();
-        }
     }
+}
---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,30 @@
+public class Grades {
+    private int[] grades;
+    private int tabElement;
+
+    public Grades(){
+        this.grades = new int[10];
+        this.tabElement = 0;
+    }
+    public void add(int value){
+        if(this.tabElement == 10){
+            return;
+        }
+        this.grades[this.tabElement] = value;
+        this.tabElement++;
+    }
+    public int[] lastGrade(){
+        if(this.tabElement>0) {
+            return grades;
+        }
+        return new int[0];
+    }
+    public double averange() {
+        int sum=0;
+        for(int i=0; i< grades.length;i++){
+            sum=sum+ grades.length;
+        } return sum/grades.length;
+
+
+    }
+}
