92423f2ed711cef4b88eda7ecd5e5d9d07fa209b
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2022-10-08 16:02:43 +0200

    lista zadan stream

--- kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -1,18 +1,43 @@
 package com.kodilla.stream;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UsersManager {
     public static void main(String[] args) {
-        processUsersStream();
+        List<String> chemistGroupUsernames = filterChemistGroupUsernames();
+        System.out.println(chemistGroupUsernames);
+
     }
-    private static void processUsersStream(){
-        UseRepository.getUserList()
+
+    public static List<String> filterChemistGroupUsernames() {
+        List<String> usernames = UseRepository.getUserList()
                 .stream()
                 .filter(user -> user.getGroup().equals("Chemists"))
                 .map(UsersManager::getUserName)
-                .forEach(username -> System.out.println(username));
+                .collect(Collectors.toList());
 
+        return usernames;
     }
-    public static String getUserName(User user){
+
+
+    public static String getUserName(User user) {
         return user.getUsername();
+    }
+
+    public static List<User> getHigherAge(int age) {
+        List<User> users = UseRepository.getUserList()
+                .stream()
+                .filter(n -> n.getAge() > age )
+                .collect(Collectors.toList());
+      return users;
+    }
+
+    public static List<User> getNumberOfPostsAndGroupFilter(int post,String group) {
+        List<User> users = UseRepository.getUserList()
+                .stream()
+                .filter(n -> n.getNumberOfPost() >= post && n.getGroup().equals(group))
+                .collect(Collectors.toList());
+        return users;
     }
 }
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/UsersManagerTest.java
@@ -0,0 +1,41 @@
+package com.kodilla.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersManagerTest {
+    private UsersManager usersManager = new UsersManager();
+    @Test
+    public void filterChemistGroupUsernamesTest(){
+        List<String> result = usersManager.filterChemistGroupUsernames();
+        List<String> expectedList = new ArrayList<>();
+        expectedList.add("Walter White");
+        expectedList.add("Gale Boetticher");
+        assertEquals(expectedList, result);
+    }
+
+    @Test
+    public void getHigherAgeTest() {
+        List<User> result = usersManager.getHigherAge(45);
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new User("Walter White", 50, 7, "Chemists"));
+        expectedList.add(new User("Gus Firing", 49, 0, "Board"));
+        expectedList.add(new User("Mike Ehrmantraut", 57, 0, "Security"));
+        assertEquals(expectedList, result);
+
+
+    }
+
+    @Test
+    public void getNumberOfPostsAndGroupFilterTest() {
+        List<User> result = usersManager.getNumberOfPostsAndGroupFilter(2,"Chemists");
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new User("Walter White", 50, 7, "Chemists"));
+        expectedList.add(new User("Gale Boetticher", 44, 2, "Chemists"));
+        assertEquals(expectedList, result);
+    }
+}
